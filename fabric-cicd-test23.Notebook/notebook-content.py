ad83f592fad418a86f2af3958ad5575286b238f6
yuvraj-singh-rathod <Yuvraj.Singh@dynatechconsultancy.com>
2026-06-26 17:42:41 +0530

    Committing 1 item from workspace 524090e0-5827-4f82-982c-596f787629d1

---
+++ fabric-cicd-test23.Notebook/notebook-content.py
@@ -0,0 +1,24 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+Hello 123
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
